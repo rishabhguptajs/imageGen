40d51ed1aa25457329960437f4d96fe26d4ec85e
rishabhguptajs <rishabhgupta4523@gmail.com>
2023-12-12 22:02:29 +0530

    committed some changes

--- server/index.js
+++ server/index.js
@@ -3,12 +3,16 @@ import * as dotenv from 'dotenv'
 import cors from 'cors'
 
 import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api/v1/post', postRoutes)
+app.use('/api/v1/dalle', dalle)
 
 app.get('/', (req, res) => {
     res.send("Hello from Remagine")
